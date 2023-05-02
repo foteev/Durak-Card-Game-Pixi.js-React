3c2ec70705da9e5b275837d9b913f55ff465f898
vivi196 <foteev@gmail.com>
2023-05-02 15:34:44 +0200

    fix: fix paths

--- src/components/Buttons/Buttons.tsx
+++ src/components/Buttons/Buttons.tsx
@@ -19,6 +19,7 @@ type Props = {
   playerIndex: number
 }
 export const Buttons = (props: any) => {
+  
   const snap = useSnapshot(gameStore);
 
   const playerIndex = props.playerIndex
@@ -44,6 +45,10 @@ export const Buttons = (props: any) => {
     // socket.emit('reset');
   }
 
+  const handleExit = () => {
+    socket.emit('exit', playerIndex);
+  }
+
   return (
     <div className="buttons-block">
       <div className="flex items-center gap-1 buttons-container">
@@ -58,20 +63,23 @@ export const Buttons = (props: any) => {
           }
         </Button>
         <Button variant="text" className="flex items-center gap-2" onClick={() => handleSort('byRank')}>
-          <ArrowsUpDownIcon strokeWidth={1} className="h-5 w-5" /> Sort by rank 
+          <ArrowsUpDownIcon strokeWidth={1} className="h-5 w-5" /> by rank 
         </Button>
         <Button variant="text" className="flex items-center gap-2" onClick={() => handleSort('bySuit')}>
-          <ArrowsUpDownIcon strokeWidth={1} className="h-5 w-5" /> sort by suit 
+          <ArrowsUpDownIcon strokeWidth={1} className="h-5 w-5" /> by suit 
         </Button>
         <Button variant="gradient" className="flex items-center gap-3" onClick={handleUndoClick}>
-          <ArrowPathIcon strokeWidth={1} className="h-5 w-5" /> Undo
+           Undo
         </Button>
         <Button variant="gradient" className="flex items-center gap-3" onClick={handleReset}>
-            Reset
+          <ArrowPathIcon strokeWidth={1} className="h-5 w-5" />  Replay
         </Button>
         <Button variant="gradient" className="flex items-center gap-3" onClick={handleEndGame}>
           <FlagIcon  strokeWidth={1} className="h-5 w-5" />  
             End game
+        </Button>
+        <Button variant="gradient" className="flex items-center gap-3" onClick={handleExit}> 
+            Exit
         </Button>
         <Button variant="gradient" className="flex items-center gap-3" onClick={() => window.location.href = 'https://github.com/foteev/Durak-Card-Game-Pixi.js-React'}>
           @github
